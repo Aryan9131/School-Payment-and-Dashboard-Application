a379bbdb258db5e9eba60a987533418a5fb53810
Aryan Nayak <aryannayak9131@gmail.com>
2025-01-24 22:21:19 +0530

    Updated userProfile Alignments

--- client/src/components/UserPage.jsx
+++ client/src/components/UserPage.jsx
@@ -5,12 +5,12 @@ import { useSelector } from 'react-redux'
 export const UserProfile = () => {
     const {userDetails} = useSelector((state)=>state.user)
   return (
-    <Box sx={{width:'100vw',height:'100vh' , backgroundColor:'whitesmoke', display:'flex',alignItems:'center', justifyContent:'center'}}>
+    <Box sx={{width:'100vw',height:'80vh' , backgroundColor:'whitesmoke', display:'flex',alignItems:'center', justifyContent:'center'}}>
         <Box sx={{width:{xs:'90%', sm:'60%', md:'50%'}, height:'50%', backgroundColor:'white', padding:'10px'}}>
-        <Typography>Name : {userDetails.name}</Typography>
-        <Typography>Email : {userDetails.email}</Typography>
-        <Typography>Number : {userDetails.number}</Typography>
-        <Typography>Password : {userDetails.password}</Typography>
+        <Typography>Name : {userDetails?.name}</Typography>
+        <Typography>Email : {userDetails?.email}</Typography>
+        <Typography>Number : {userDetails?.number}</Typography>
+        <Typography>Password : {userDetails?.password}</Typography>
         </Box>
     </Box>
   )
